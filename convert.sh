01f9f2931dab03d605a9a0fd4680e1f8e72cf0e1
Vitaliy <vitaliy.kuzmich@gmail.com>
2018-11-25 20:17:44 +0200

    thumbs

--- convert.sh
+++ convert.sh
@@ -5,7 +5,7 @@ if [ $3 = 'image' ]; then
 elif [ $3 = 'video' ]; then
   echo " ffmpeg $1"
   ffmpeg -v warning -ss 0 -t 10 -i "$1" -vf fps=10,scale=100:-1 -gifflags -transdiff -n "$2"
-else if [ $3 != 'audio' ]; then
+elif [ $3 != 'audio' ]; then
   echo " convert $1"
   convert -background White -alpha Background -resize 100x100 -flatten "$1" "$2"
 fi
