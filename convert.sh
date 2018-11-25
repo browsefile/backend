97ab5e9837647893e12f8cc5021189e28e2e49f5
Vitaliy <vitaliy.kuzmich@gmail.com>
2018-11-25 21:31:09 +0200

    filter out text|audio

--- convert.sh
+++ convert.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [ $3 = 'image' ]; then
-  echo " vipsthumbnail $1"
+if [ "$3" = 'image' ]; then
+  #echo " vipsthumbnail $1"
   vipsthumbnail -s 100 "$1" -o "$2"
-elif [ $3 = 'video' ]; then
-  echo " ffmpeg $1"
+elif [ "$3" = 'video' ]; then
+  #echo " ffmpeg $1"
   ffmpeg -v warning -ss 0 -t 10 -i "$1" -vf fps=10,scale=100:-1 -gifflags -transdiff -n "$2"
-elif [ $3 != 'audio' ]; then
-  echo " convert $1"
+elif [ "$3" != 'audio' ] && [ "$3" != 'text' ]; then
+  #echo " convert $1"
   convert -background White -alpha Background -resize 100x100 -flatten "$1" "$2"
 fi
